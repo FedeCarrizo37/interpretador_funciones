79f30caa5fd63dd3d2a5ea46f6977af4403cf886
nahu_ <nahu_@EXO-G52A>
2024-08-28 15:52:02 -0300

    Agregar archivos de proyecto.

---
+++ interpretador_funciones/Interprete.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interpretador_funciones
+{
+    public class Interprete
+    {
+        public static string[] DividirPolinomio1(string polinomio)
+        {
+            // Dividir el string en términos individuales utilizando los delimitadores "+" y "-"
+            string[] terminos = polinomio.Split(new char[] { '+', '-' },StringSplitOptions.RemoveEmptyEntries);
+
+            // Recorrer cada término y eliminar espacios en blanco
+            for (int i = 0; i < terminos.Length; i++)
+            {
+                terminos[i] = terminos[i].Trim();
+                Console.WriteLine(terminos.Length);
+            }
+
+            return terminos;
+        }
+
+        public static List<string> DividirPolinomio2(string polinomio)
+        {
+            List<string> terminos = new List<string>();
+            int char_inicio = 0, char_final = 0, indice_char = 0;
+            foreach (char c in polinomio)
+            {
+                if(c == '+' || c == '-')
+                {
+                    char_final = indice_char;
+                    if(char_inicio != char_final)
+                    {
+                        terminos.Add(polinomio.Substring(char_inicio, char_final-char_inicio));
+                        char_inicio = char_final;
+                    }
+                }
+                indice_char += 1;
+                if (indice_char == polinomio.Length) 
+                {
+                    terminos.Add(polinomio.Substring(char_inicio, polinomio.Length-char_inicio));
+                }
+            }
+
+            return terminos;
+        }
+
+        public static string QuitarX(string termino) 
+        {
+            string termino_sin_x = "";
+            //esto recibe un termino con formato 4x^4 y le quita lo x^4
+            if (termino.ToLower().IndexOf("x") != -1)
+            {
+                termino_sin_x = termino.Substring(0, termino.ToLower().IndexOf("x"));
+            }
+            else
+            {
+                termino_sin_x = termino;
+            }
+            return termino_sin_x;
+        }
+
+        public static List<int> CalcularDivisores(int ti)
+        {
+            List<int> divisores = new List<int>();
+
+            // Calcular divisores
+            for (int i = 1; i <= ti; i++)
+            {
+                if (ti % i == 0)
+                {
+                    divisores.Add(i); // Agregar el divisor positivo a la lista
+                    divisores.Add(-i); // Agregar el divisor negativo a la lista
+                }
+            }
+
+            return divisores;
+        }
+    }
+}
---
+++ interpretador_funciones/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interpretador_funciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.Write("dame un numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine().ToString());
+
+            List<int> divisores = new List<int>();
+            divisores = Interprete.CalcularDivisores(numero);
+
+            Console.WriteLine("Divisores de {0}", numero);
+            foreach (int i in divisores) 
+            { 
+                Console.WriteLine(i);
+            }
+            */
+
+            Console.WriteLine("*************************************");
+            Console.Write("ahora dame un polinomio:");
+            string polinomio;
+            string[] terminos_del_polinomio_array;
+            List<string> terminos_del_polinomio_list = new List<string>();
+            polinomio = Console.ReadLine().ToString();
+
+            terminos_del_polinomio_array = Interprete.DividirPolinomio1(polinomio);
+            Console.WriteLine("division en terminos con metodologia 1: ");
+            foreach (string coso in terminos_del_polinomio_array)
+            {
+                Console.WriteLine("=>" + coso);
+            }
+
+            Console.WriteLine("division en terminos con metodologia 2: ");
+            terminos_del_polinomio_list = Interprete.DividirPolinomio2(polinomio);
+            foreach (string coso in terminos_del_polinomio_list)
+            {
+                Console.WriteLine("=>" + coso);
+            }
+            Console.WriteLine("division en terminos con metodologia 2 (sacandole la X): ");
+            foreach (string coso in terminos_del_polinomio_list)
+            {
+                Console.WriteLine("=>" + Interprete.QuitarX(coso));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
